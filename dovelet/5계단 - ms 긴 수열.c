d26709ae8808368d4c3e61e165603a3d84033641
Daehyeon Kim <rhealove01@gmail.com>
2021-06-10 18:15:25 +0900

    5계단 - ms 긴 수열

---
+++ dovelet/5계단 - ms 긴 수열.c
@@ -0,0 +1,6 @@
+m,h,l,i,p;main(a){for(;~scanf("%d",&a);p=a,m=m<l|m<h?h<l?l:h:m)i++?(p<a?h=0:++h,p>a?l=0:++l):0;printf("%d",i-2?m+1:1);}
+
+/*
+ * code len: 119
+ * 가장 긴 증가/감소 수열의 길이를 배열을 입력 받지 않고 찾았다.
+ */
