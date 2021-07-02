08f423fa246f2b28e0ac0ce37c3e9503d9cbd625
Daehyeon Kim <rhealove01@gmail.com>
2021-07-03 02:40:19 +0900

    6계단 - rank1 sort 후 rank

---
+++ dovelet/6계단 - rank1 sort 후 rank(ShortCode).c
@@ -0,0 +1,6 @@
+i,s,a['┌'];c(int*a){return *a-*1[&a];}main(p){for(;~scanf("%d",i++?a+i:&p););qsort(a,i,4,c);for(;--i-1;printf("%d %d\n",a[i],a[i]-a[i+1]?p=s:p))++s;}
+
+/*
+ * code len: 150
+ * 최단 코드 qsort
+ */
