94a65d7344582d96be43b3479a312d2152288ec7
Daehyeon Kim <rhealove01@gmail.com>
2021-07-03 02:41:32 +0900

    6계단 - rank1 sort 후 rank

--- dovelet/6계단 - rank1 sort 후 rank(ShortCode).c
+++ dovelet/6계단 - rank1 sort 후 rank(ShortCode).c
@@ -1,6 +1,6 @@
-i,s,a['¦£'];c(int*a){return *a-*1[&a];}main(p){for(;~scanf("%d",i++?a+i:&p););qsort(a,i,4,c);for(;--i-1;printf("%d %d\n",a[i],a[i]-a[i+1]?p=s:p))++s;}
+i,s,a['â”Œ'];c(int*a){return *a-*1[&a];}main(p){for(;~scanf("%d",i++?a+i:&p););qsort(a,i,4,c);for(;--i-1;printf("%d %d\n",a[i],a[i]-a[i+1]?p=s:p))++s;}
 
 /*
  * code len: 150
- * ÃÖ´Ü ÄÚµå qsort
+ * ìµœë‹¨ ì½”ë“œ qsort
  */
